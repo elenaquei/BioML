29137e59cd66b8dc33c010e8b5be94a11b1c96f7
Elena <elenaquei@hotmail.it>
2024-04-03 15:29:31 +0200

    reformatted - no change

--- models/neural_odes.py
+++ models/neural_odes.py
@@ -8,9 +8,10 @@ import torch
 import torch.nn as nn
 from torchdiffeq import odeint, odeint_adjoint
 from warnings import warn
+
 # from adjoint_neural_ode import adj_Dynamics
 
-#odeint Returns:
+# odeint Returns:
 #         y: Tensor, where the first dimension corresponds to different
 #             time points. Contains the solved value of y for each desired time point in
 #             `t`, with the initial value `y0` being the first element along the first
@@ -20,10 +21,10 @@ from warnings import warn
 MAX_NUM_STEPS = 1000
 
 
-
 def tanh_prime(input):
-    
-    return 1 - torch.tanh(input) * torch.tanh(input) # use torch.sigmoid to make sure that we created the most efficient implemetation based on builtin PyTorch functions
+    return 1 - torch.tanh(input) * torch.tanh(
+        input)  # use torch.sigmoid to make sure that we created the most efficient implemetation based on builtin PyTorch functions
+
 
 # create a class wrapper from PyTorch nn.Module, so
 # the function now can be easily used in models
@@ -43,35 +44,38 @@ class Tanh_Prime(nn.Module):
         >>> input = torch.randn(2)
         >>> output = m(input)
     '''
+
     def __init__(self):
         '''
         Init method.
         '''
-        super().__init__() # init the base class
+        super().__init__()  # init the base class
 
     def forward(self, input):
         '''
         Forward pass of the function.
         '''
-        return tanh_prime(input) # simply apply already implemented SiLU
+        return tanh_prime(input)  # simply apply already implemented SiLU
 
 
 # Useful dicos:
 activations = {'tanh': nn.Tanh(),
-                'relu': nn.ReLU(),
-                'sigmoid': nn.Sigmoid(),
-                'leakyrelu': nn.LeakyReLU(negative_slope=0.25, inplace=True),
-                'tanh_prime': tanh_prime
-}
+               'relu': nn.ReLU(),
+               'sigmoid': nn.Sigmoid(),
+               'leakyrelu': nn.LeakyReLU(negative_slope=0.25, inplace=True),
+               'tanh_prime': tanh_prime
+               }
 architectures = {'inside': -1, 'outside': 0, 'bottleneck': 1}
+
 
 class Dynamics(nn.Module):
     """
     The nonlinear, right hand side $f(u(t), x(t)) of the neural ODE.
     We distinguish the different structures defined in the dictionary "architectures" just above.
     """
-    def __init__(self, device, data_dim, hidden_dim, augment_dim=0, 
-                non_linearity='tanh', architecture='inside', T=10, time_steps=10):
+
+    def __init__(self, device, data_dim, hidden_dim, augment_dim=0,
+                 non_linearity='tanh', architecture='inside', T=10, time_steps=10):
         super(Dynamics, self).__init__()
         self.device = device
         self.augment_dim = augment_dim
@@ -81,7 +85,7 @@ class Dynamics(nn.Module):
 
         if non_linearity not in activations.keys() or architecture not in architectures.keys():
             raise ValueError("Activation function or architecture not found. Please reconsider.")
-        
+
         self.non_linearity = activations[non_linearity]
         self.architecture = architectures[architecture]
         self.T = T
@@ -90,51 +94,52 @@ class Dynamics(nn.Module):
         if self.architecture > 0:
             ##-- R^{d_aug} -> R^{d_hid} layer -- 
             blocks1 = [nn.Linear(self.input_dim, hidden_dim) for _ in range(self.time_steps)]
-            self.fc1_time = nn.Sequential(*blocks1) 
+            self.fc1_time = nn.Sequential(*blocks1)
             ##-- R^{d_hid} -> R^{d_aug} layer --
             blocks3 = [nn.Linear(hidden_dim, self.input_dim) for _ in range(self.time_steps)]
             self.fc3_time = nn.Sequential(*blocks3)
 
-            blocks_gamma = [nn.Linear(self.input_dim,hidden_dim) for _ in range(self.time_steps)]
+            blocks_gamma = [nn.Linear(self.input_dim, hidden_dim) for _ in range(self.time_steps)]
             self.gamma = nn.Sequential(*blocks_gamma)
         else:
             ##-- R^{d_hid} -> R^{d_hid} layer --
             blocks = [nn.Linear(hidden_dim, hidden_dim) for _ in range(self.time_steps)]
             self.fc2_time = nn.Sequential(*blocks)
-        
-    def forward(self, t, x, prnt = False):
+
+    def forward(self, t, x, prnt=False):
         """
         The output of the class -> f(x(t), u(t)).
         f(x(t), u(t)) = f(x,u^k)
         """
-        dt = self.T/self.time_steps   #here was no -1 before which does not fit with adjoint solver otherwise
-        k = int(t/dt)
-        if k >self.T-1:
+        dt = self.T / self.time_steps  # here was no -1 before which does not fit with adjoint solver otherwise
+        k = int(t / dt)
+        if k > self.T - 1:
             warn('Extending the dynamics')
-            k = self.T-1 # here, the dynamics is defined to "continue" with the latest values
-        
+            k = self.T - 1  # here, the dynamics is defined to "continue" with the latest values
+
         if self.architecture < 1:
             w_t = self.fc2_time[k].weight
             b_t = self.fc2_time[k].bias
-            if self.architecture < 0:                               # w(t)\sigma(x(t))+b(t)  inner
-                out = self.non_linearity(x).matmul(w_t.t()) + b_t        
-            else:                                                   # \sigma(w(t)x(t)+b(t))   outer
-                out = self.non_linearity(x.matmul(w_t.t())+b_t)
-        else:                                                       # w1(t)\sigma(w2(t)x(t)+b2(t))+b1(t) bottle-neck
+            if self.architecture < 0:  # w(t)\sigma(x(t))+b(t)  inner
+                out = self.non_linearity(x).matmul(w_t.t()) + b_t
+            else:  # \sigma(w(t)x(t)+b(t))   outer
+                out = self.non_linearity(x.matmul(w_t.t()) + b_t)
+        else:  # w1(t)\sigma(w2(t)x(t)+b2(t))+b1(t) bottle-neck
             w1_t = self.fc1_time[k].weight
             b1_t = self.fc1_time[k].bias
             w2_t = self.fc3_time[k].weight
             b2_t = self.fc3_time[k].bias
             gam = self.gamma[k].bias
             out = self.non_linearity(x.matmul(w1_t.t()) + b1_t)
-            #out = 2*self.non_linearity(x.matmul(w1_t.t()) + 2)
-            #out = 2*self.non_linearity(x.matmul(torch.tensor([[0.0,-1.0],[-1.0,0.0]])) + 2)
+            # out = 2*self.non_linearity(x.matmul(w1_t.t()) + 2)
+            # out = 2*self.non_linearity(x.matmul(torch.tensor([[0.0,-1.0],[-1.0,0.0]])) + 2)
             out = out.matmul(w2_t.t()) + b2_t
-            #out = out + 2
-            out = out - gam*x
-            #out = out - x
+            # out = out + 2
+            out = out - gam * x
+            # out = out - x
 
         return out
+
 
 class Semiflow(nn.Module):  # this should allow to calculate the flow for dot(x) = f(u,x) AND dot(p) = -Dxf(u,x)p
     """
@@ -146,9 +151,10 @@ class Semiflow(nn.Module):  # this should allow to calculate the flow for dot(x)
     - dynamics denotes the instance of the class Dynamics, defining the dynamics f(x,u)
     ***
     """
+
     def __init__(self, device, dynamics, tol=1e-3, adjoint=False, T=10, time_steps=10):
         super(Semiflow, self).__init__()
-        self.adjoint = adjoint 
+        self.adjoint = adjoint
         self.device = device
         self.dynamics = dynamics
         self.tol = tol
@@ -156,33 +162,34 @@ class Semiflow(nn.Module):  # this should allow to calculate the flow for dot(x)
         self.time_steps = time_steps
 
     def forward(self, x, eval_times=None):
-    
-        dt = self.T/self.time_steps
+
+        dt = self.T / self.time_steps
 
         if eval_times is None:
             integration_time = torch.tensor([0, self.T]).float().type_as(x)
         else:
             integration_time = eval_times.type_as(x)
 
-        if self.adjoint:  
-            #out = odeint_adjoint(self.dynamics, x_aug, integration_time, method='euler', options={'step_size': dt})
-            out = odeint_adjoint(self.dynamics, x, integration_time, method='dopri5', rtol = 0.001, atol = 0.001)
-        
+        if self.adjoint:
+            # out = odeint_adjoint(self.dynamics, x_aug, integration_time, method='euler', options={'step_size': dt})
+            out = odeint_adjoint(self.dynamics, x, integration_time, method='dopri5', rtol=0.001, atol=0.001)
+
         else:
             # out = odeint(self.dynamics, x_aug, integration_time, method='euler', options={'step_size': dt})
-            out = odeint(self.dynamics, x, integration_time, method='dopri5', rtol = 0.001, atol = 0.001)
-            
-            #i need to put the out into the odeint for the adj_out
+            out = odeint(self.dynamics, x, integration_time, method='dopri5', rtol=0.001, atol=0.001)
+
+            # i need to put the out into the odeint for the adj_out
             # adj_out = odeint(self.adj_dynamics, torch.eye(x.shape[0]), torch.flip(integration_time,[0]), method='euler', options={'step_size': dt}) #this is new for the adjoint
-            
+
         if eval_times is None:
-            return out[1] 
+            return out[1]
         else:
             return out
 
     def trajectory(self, x, timesteps):
         integration_time = torch.linspace(0., self.T, timesteps)
         return self.forward(x, eval_times=integration_time)
+
 
 class NeuralODE(nn.Module):
     """
@@ -196,18 +203,19 @@ class NeuralODE(nn.Module):
     - fixed_projector is a boolean indicating whether the output layer is trained or not
     ***
     """
+
     def __init__(self, device, data_dim, hidden_dim, output_dim=2,
                  augment_dim=0, non_linearity='tanh',
-                 tol=1e-3, adjoint=False, architecture='inside', 
-                 T=10, time_steps=10, 
-                 cross_entropy=True, fixed_projector=False, reduced_dynamics = False):
+                 tol=1e-3, adjoint=False, architecture='inside',
+                 T=10, time_steps=10,
+                 cross_entropy=True, fixed_projector=False, reduced_dynamics=False):
         super(NeuralODE, self).__init__()
         self.device = device
         self.data_dim = data_dim
         self.hidden_dim = hidden_dim
         self.augment_dim = augment_dim
-        if output_dim == 1 and cross_entropy: 
-            #output_dim = 1 pour MSE; >=2 pour cross entropy for binary classification.
+        if output_dim == 1 and cross_entropy:
+            # output_dim = 1 pour MSE; >=2 pour cross entropy for binary classification.
             raise ValueError('Incompatible output dimension with loss function.')
         self.output_dim = output_dim
         self.tol = tol
@@ -216,15 +224,16 @@ class NeuralODE(nn.Module):
         self.architecture = architecture
         self.cross_entropy = cross_entropy
         self.fixed_projector = fixed_projector
-        dynamics = Dynamics(device, data_dim, hidden_dim, augment_dim, non_linearity, architecture, self.T, self.time_steps)
-        
-        self.flow = Semiflow(device, dynamics, tol, adjoint, T,  time_steps) #, self.adj_flow
+        dynamics = Dynamics(device, data_dim, hidden_dim, augment_dim, non_linearity, architecture, self.T,
+                            self.time_steps)
+
+        self.flow = Semiflow(device, dynamics, tol, adjoint, T, time_steps)  # , self.adj_flow
         self.linear_layer = nn.Linear(self.flow.dynamics.input_dim,
-                                         self.output_dim)
-        self.non_linearity = nn.Tanh() #not really sure why this is here
-        
+                                      self.output_dim)
+        self.non_linearity = nn.Tanh()  # not really sure why this is here
+
     def forward(self, x, return_features=True):
-        
+
         features = self.flow(x)
         self.traj = self.flow.trajectory(x, self.time_steps)
         pred = self.linear_layer(features)
@@ -232,8 +241,7 @@ class NeuralODE(nn.Module):
         if not self.cross_entropy:
             pred = self.non_linearity(pred)
             self.proj_traj = self.non_linearity(self.proj_traj)
-        
+
         if return_features:
             return features, pred
         return pred, self.proj_traj
-    
