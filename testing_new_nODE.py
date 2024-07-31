5d33c61265666dabd66a42d7a1916d401617ae64
Lucas Jansen Klomp <l.f.jansenklomp@utwente.nl>
2024-07-31 12:11:13 +0200

    made changing batch size an option

--- testing_new_nODE.py
+++ testing_new_nODE.py
@@ -9,16 +9,18 @@ device = 'cpu'#torch.device('cuda' if torch.cuda.is_available() else 'cpu')''
 ODE_dim = 2
 Gamma = np.array([-1., -1.])
 integration_time = 3
-Win = np.array([[0, -1.], [-2., 0]])
+Win = np.array([[0, -1.], [-1., 0]])
+Wout = 2*np.eye(2)
 bin = np.array([[2.], [2.]])
+bout = np.array([[2.], [2.]])
 
-node2 = make_nODE_from_parameters(Gamma, Win=Win, bin=bin)
+node2 = make_nODE_from_parameters(Gamma, Win=Win, bin=bin, Wout=Wout, bout=bout)
 node2.plot()
 print('real Toggle Switch')
 print(node2)
 
 
-train_data, test_data = weights_to_dataset(integration_time, Gamma, Win=Win, bin=bin)
+train_data, test_data = weights_to_dataset(integration_time, Gamma, Win=Win, bin=bin, Wout=Wout, bout=bout, batch_size = 5)
 
 node = nODE(ODE_dim, architecture='inside_weights', time_interval=[0, integration_time])
 
@@ -26,7 +28,7 @@ optimizer_node = torch.optim.Adam(node.parameters(), lr=1e-1)
 
 trainer = easyTrainer(node, optimizer_node, device)
 
-trainer.train(train_data, 50)
+trainer.train(train_data, 500)
 
 node.plot()
 print(node)
